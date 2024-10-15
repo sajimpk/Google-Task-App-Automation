2889a393865a9d6c020e36103f56cae6430faca4
sajimpk <sajimpk520@gmail.com>
2024-10-15 12:27:20 +0600

    add close Driver

--- src/main/java/org/example/setting.java
+++ src/main/java/org/example/setting.java
@@ -6,34 +6,39 @@ import io.appium.java_client.android.AndroidDriver;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.openqa.selenium.By;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class setting {
-//    @Test
-//    public void sampleTest() {
-//
-//    }
-    AndroidDriver driver;
+
+    private AndroidDriver driver;
 
 
-
-    public static <MobileElement> void main(String[] args) throws MalformedURLException {
+    @Before
+    public <MobileElement> void main() throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("platformName", "Android");
         caps.setCapability("deviceName", "pixel"); // Change to your emulator/device
         caps.setCapability("app", "C:\\Users\\MDS\\Downloads\\Google_Tasks.apk");
 
         // Initialize AndroidDriver
-        AndroidDriver driver = new AndroidDriver(
-                new URL("http://127.0.0.1:4723/wd/hub"), caps);
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
 
         // Perform some actions
         System.out.println("App launched successfully!");
-//        driver.quit();
+    }
+    @Test
+    public void sampleTest() {
+        System.out.println("App sampleTest successfully!");
+    }
 
+    @After
+    public void tearDown() {
+        driver.quit();
+        System.out.println("App Closed successfully!");
     }
 
 }
